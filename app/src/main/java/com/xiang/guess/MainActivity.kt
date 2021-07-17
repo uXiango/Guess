a359278038f71d0b89b7c5898542e0549949abd3
uXiango <k23589612@gmail.com>
2021-07-17 23:44:23 +0800

    Initial commit

---
+++ app/src/main/java/com/xiang/guess/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xiang.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.drawable.study
+        R.string.ok
+        R.color.messagecolor
+    }
+}
